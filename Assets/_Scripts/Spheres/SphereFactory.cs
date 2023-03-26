39c35524b20655be99403bdaf29758c318bb03cb
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2023-03-26 13:41:52 +0200

    Corrected wave spawn in Sphere Factory

--- Assets/_Scripts/Spheres/SphereFactory.cs
+++ Assets/_Scripts/Spheres/SphereFactory.cs
@@ -8,11 +8,21 @@ public class SphereFactory : MonoBehaviour
     public static SphereFactory instance;
     [SerializeField] private Transform spheresParent;
 
-    public List<SphereData> spheres = new List<SphereData>();
+    public List<GameObject> spheres = new List<GameObject>();
+    public Dictionary<string, GameObject> pairSpheres = new Dictionary<string, GameObject>();
 
     public Transform[] enemyPoints;
 
     [SerializeField] private int randomEnemyPoint = 1;
+
+    [Header("Spheres Names")]
+    [SerializeField] private string simpleSphere = "SimpleSphere";
+    [SerializeField] private string armoredOrangeSphere = "ArmoredOrangeSphere";
+    [SerializeField] private string armoredGreySphere = "ArmoredGreySphere";
+    [SerializeField] private string newspaperSphere = "NewspaperSphere";
+    [SerializeField] private string polemanSphere = "PolemanSphere";
+    [SerializeField] private string boxExplodeSphere = "BoxExplodeSphere";
+    [SerializeField] private string armoredSportmanSphere = "ArmoredSportmanSphere";
 
     void Awake()
     {
@@ -29,6 +39,7 @@ public class SphereFactory : MonoBehaviour
 
     private void Start()
     {
+        AddSpheresToDictionary();
         StartCoroutine(Wave0());
     }
 
@@ -40,22 +51,22 @@ public class SphereFactory : MonoBehaviour
     private IEnumerator Wave0()
     {
         yield return new WaitForSeconds(20);
-        CreateSphere(spheres[0], 1);
+        CreateSphere(pairSpheres[simpleSphere], 1);
         yield return new WaitForSeconds(20);
-        CreateSphere(spheres[0], 1);
+        CreateSphere(pairSpheres[simpleSphere], 1);
         yield return new WaitForSeconds(20);
-        CreateSphere(spheres[1], 1);
+        CreateSphere(pairSpheres[armoredOrangeSphere], 1);
         yield return new WaitForSeconds(20);
-        CreateSphere(spheres[0], 2);
+        CreateSphere(pairSpheres[simpleSphere], 2);
         yield return new WaitForSeconds(20);
-        CreateSphere(spheres[0], 2);
+        CreateSphere(pairSpheres[simpleSphere], 2);
         yield return new WaitForSeconds(20);
-        CreateSphere(spheres[0], 3);
-        CreateSphere(spheres[3], 1);
+        CreateSphere(pairSpheres[simpleSphere], 3);
+        CreateSphere(pairSpheres[newspaperSphere], 1);
         yield return new WaitForSeconds(20);
-        CreateSphere(spheres[0], 2);
-        CreateSphere(spheres[2], 1);
-        CreateSphere(spheres[1], 1);
+        CreateSphere(pairSpheres[simpleSphere], 2);
+        CreateSphere(pairSpheres[armoredGreySphere], 1);
+        CreateSphere(pairSpheres[armoredOrangeSphere], 1);
         yield return new WaitForSeconds(20);
         StartCoroutine(Wave1());
         StopCoroutine(Wave0());
@@ -65,33 +76,86 @@ public class SphereFactory : MonoBehaviour
     private IEnumerator Wave1()
     {
         yield return new WaitForSeconds(10);
-        CreateSphere(spheres[0], 15);
-        CreateSphere(spheres[1], 5);
-        CreateSphere(spheres[2], 2);
+        CreateSphere(pairSpheres[simpleSphere], 15);
+        CreateSphere(pairSpheres[armoredOrangeSphere], 5);
+        CreateSphere(pairSpheres[armoredGreySphere], 2);
+        CreateSphere(pairSpheres[newspaperSphere], 2);
         yield return new WaitForSeconds(5);
-        CreateSphere(spheres[0], 1);
-        yield return new WaitForSeconds(4);
+        CreateSphere(pairSpheres[armoredGreySphere], 1);
+        CreateSphere(pairSpheres[simpleSphere], 5);
+        CreateSphere(pairSpheres[armoredSportmanSphere], 1);
+        CreateSphere(pairSpheres[polemanSphere], 2);
+        CreateSphere(pairSpheres[simpleSphere], 5);
+        CreateSphere(pairSpheres[armoredSportmanSphere], 2);
+        CreateSphere(pairSpheres[newspaperSphere], 2);
+        CreateSphere(pairSpheres[boxExplodeSphere], 2);
+        yield return new WaitForSeconds(10);
         CreateSphere(spheres[1], 1);
+        CreateSphere(pairSpheres[simpleSphere], 4);
+        CreateSphere(pairSpheres[simpleSphere], 2);
+        CreateSphere(pairSpheres[simpleSphere], 6);
+        CreateSphere(pairSpheres[armoredSportmanSphere], 1);
+        CreateSphere(pairSpheres[armoredSportmanSphere], 1);
+        CreateSphere(pairSpheres[boxExplodeSphere], 2);
+        CreateSphere(pairSpheres[armoredSportmanSphere], 1);
         yield return new WaitForSeconds(5);
-        CreateSphere(spheres[0], 2);
-        yield return new WaitForSeconds(3);
-        CreateSphere(spheres[0], 2);
+        CreateSphere(pairSpheres[armoredSportmanSphere], 2);
+        CreateSphere(pairSpheres[armoredSportmanSphere], 1);
+        CreateSphere(pairSpheres[armoredSportmanSphere], 1);
+        CreateSphere(pairSpheres[boxExplodeSphere], 1);
+        CreateSphere(pairSpheres[boxExplodeSphere], 4);
+        CreateSphere(pairSpheres[armoredGreySphere], 3);
+        CreateSphere(pairSpheres[armoredGreySphere], 2);
+        yield return new WaitForSeconds(10);
+        CreateSphere(pairSpheres[newspaperSphere], 3);
+        CreateSphere(pairSpheres[armoredSportmanSphere], 1);
+        CreateSphere(pairSpheres[polemanSphere], 1);
+        CreateSphere(pairSpheres[newspaperSphere], 1);
+        CreateSphere(pairSpheres[armoredSportmanSphere], 1);
         yield return new WaitForSeconds(5);
-        CreateSphere(spheres[0], 3);
-        yield return new WaitForSeconds(2);
-        CreateSphere(spheres[0], 4);
+        CreateSphere(pairSpheres[armoredSportmanSphere], 2);
+        CreateSphere(pairSpheres[armoredSportmanSphere], 1);
+        CreateSphere(pairSpheres[polemanSphere], 1);
+        CreateSphere(pairSpheres[armoredSportmanSphere], 1);
+        CreateSphere(pairSpheres[boxExplodeSphere], 1);
+        CreateSphere(pairSpheres[boxExplodeSphere], 4);
+        CreateSphere(pairSpheres[armoredGreySphere], 3);
+        CreateSphere(pairSpheres[armoredGreySphere], 2);
+        CreateSphere(pairSpheres[polemanSphere], 1);
+        yield return new WaitForSeconds(10);
+        CreateSphere(pairSpheres[simpleSphere], 15);
+        CreateSphere(pairSpheres[armoredOrangeSphere], 10);
+        CreateSphere(pairSpheres[armoredGreySphere], 7);
+        CreateSphere(pairSpheres[newspaperSphere], 2);
+        CreateSphere(pairSpheres[newspaperSphere], 5);
+        CreateSphere(pairSpheres[armoredSportmanSphere], 3);
+        CreateSphere(pairSpheres[armoredSportmanSphere], 6);
+        CreateSphere(pairSpheres[polemanSphere], 2);
+        CreateSphere(pairSpheres[armoredSportmanSphere], 3);
+        CreateSphere(pairSpheres[polemanSphere], 1);
         yield return new WaitForSeconds(5);
 
 
     }
 
-    private void CreateSphere(SphereData sphere, int spheresAmount)
+    private void CreateSphere(GameObject sphere, int spheresAmount)
     {
         for(int i=0; i < spheresAmount; i++)
         {
             randomEnemyPoint = Random.Range(0, 5);
-            GameObject newSphere = Instantiate(sphere.spherePrefab, enemyPoints[randomEnemyPoint].position, enemyPoints[randomEnemyPoint].rotation, spheresParent);
+            GameObject newSphere = Instantiate(sphere, enemyPoints[randomEnemyPoint].position, enemyPoints[randomEnemyPoint].rotation, spheresParent);
         }
+    }
+
+    private void AddSpheresToDictionary()
+    {
+        pairSpheres.Add(simpleSphere, spheres[0]);
+        pairSpheres.Add(armoredOrangeSphere, spheres[1]);
+        pairSpheres.Add(armoredGreySphere, spheres[2]);
+        pairSpheres.Add(newspaperSphere, spheres[3]);
+        pairSpheres.Add(polemanSphere, spheres[4]);
+        pairSpheres.Add(boxExplodeSphere, spheres[5]);
+        pairSpheres.Add(armoredSportmanSphere, spheres[6]);
     }
 
 
